5881fb4266259c9a0c86ca8b943eb8a09c3e216a
Niksan Karkee <niksankarkee@Niksans-MacBook-Pro.local>
2024-02-13 23:23:55 +0900

    import lumbook

--- src/main/java/com/niksan/niksansocialmedia/response/ApiResponse.java
+++ src/main/java/com/niksan/niksansocialmedia/response/ApiResponse.java
@@ -1,32 +1,16 @@
 package com.niksan.niksansocialmedia.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ApiResponse {
 
     private String message;
 
     private boolean status;
 
-    public ApiResponse() {
-    }
-
-    public ApiResponse(String message, boolean status) {
-        this.message = message;
-        this.status = status;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
-
-    public boolean isStatus() {
-        return status;
-    }
-
-    public void setStatus(boolean status) {
-        this.status = status;
-    }
 }
