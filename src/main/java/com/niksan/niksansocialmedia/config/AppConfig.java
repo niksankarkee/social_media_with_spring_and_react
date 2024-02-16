77838bec78111bb3c7090bfcaef4300743cb4776
Niksan Karkee <niksankarkee@Niksans-MacBook-Pro.local>
2024-02-16 22:34:20 +0900

    implemented cors

--- src/main/java/com/niksan/niksansocialmedia/config/AppConfig.java
+++ src/main/java/com/niksan/niksansocialmedia/config/AppConfig.java
@@ -1,5 +1,6 @@
 package com.niksan.niksansocialmedia.config;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -10,6 +11,12 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -25,8 +32,29 @@ public class AppConfig {
                                             .anyRequest()
                                             .permitAll())
                         .addFilterBefore(new JwtValidator(), BasicAuthenticationFilter.class)
-                        .csrf(AbstractHttpConfigurer::disable);
+                        .csrf(AbstractHttpConfigurer::disable)
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()));
         return http.build();
+    }
+
+    private CorsConfigurationSource corsConfigurationSource() {
+        return new CorsConfigurationSource() {
+            @Override
+            public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+                CorsConfiguration cfg = new CorsConfiguration();
+                cfg.setAllowedOrigins(List.of(
+                        "localhost:3000/"
+                ));
+                cfg.setAllowedMethods(Collections.singletonList("*"));
+                cfg.setAllowCredentials(true);
+                cfg.setAllowedHeaders(Collections.singletonList("*"));
+                cfg.setExposedHeaders(List.of(
+                        "Authorization"
+                ));
+                cfg.setMaxAge(3600L);
+                return cfg;
+            }
+        };
     }
 
     @Bean
